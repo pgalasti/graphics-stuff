b8e10a491b1fd6071d1aa04bd64d75b9e12a37f8
Paul Galasti <pgalasti@gmail.com>
2026-08-07 22:30:22 -0400

    Render a simple triangle

---
+++ opengl/common/defines.h
@@ -0,0 +1,6 @@
+#ifndef G_DEFINES
+#define G_DEFINES
+
+using ObjID = unsigned int;
+
+#endif
--- opengl/triangle/main.cpp
+++ opengl/triangle/main.cpp
@@ -1,10 +1,36 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "../common/defines.h"
+
 #include <iostream>
+#include <string>
 
 void frameBufferSize_callback(GLFWwindow* pWindow, int width, int height);
 void handleInput(GLFWwindow* pWindow);
+bool isShaderCompileSuccessful(const ObjID shaderId);
+bool isProgramLinkSuccessful(const ObjID shaderId);
+
+
+constexpr float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};  
+
+const std::string vsSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main(){\n"
+"  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}";
+
+const std::string fsSource = 
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main() {\n"
+"  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}";
 
 int main(int argc, char* argv[]) {
 
@@ -32,10 +58,62 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "OpenGL Version Loaded: " << glGetString(GL_VERSION) << std::endl;
 
+
+  // Vertex Shader 
+  ObjID vertexShader { glCreateShader(GL_VERTEX_SHADER) };
+  const char* vsCodePtr = vsSource.c_str();
+  glShaderSource(vertexShader, 1, &vsCodePtr, nullptr);
+  glCompileShader(vertexShader);
+  if(!isShaderCompileSuccessful(vertexShader)) {
+    return 1;
+  }
+
+  // Fragment shader
+  ObjID fragmentShader { glCreateShader(GL_FRAGMENT_SHADER) };
+  const char* fsCodePtr = fsSource.c_str();
+  glShaderSource(fragmentShader, 1, &fsCodePtr, nullptr);
+  glCompileShader(fragmentShader);
+  if(!isShaderCompileSuccessful(fragmentShader)) {
+    return 1;
+  }
+
+  // Create the program
+  ObjID shaderProgram { glCreateProgram() };
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  if(!isProgramLinkSuccessful(shaderProgram)) {
+    return 1;
+  }
+
+  ObjID VAO;
+  ObjID VBO;
+
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  
+  // Use it and clean up shader object handlers
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  glBindVertexArray(VAO);
+  
   while(!glfwWindowShouldClose(pWindow)) {
     handleInput(pWindow);
     glfwSwapBuffers(pWindow);
     glfwPollEvents();
+
+    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
   }
 
   glfwTerminate();
@@ -52,4 +130,32 @@ void handleInput(GLFWwindow* pWindow) {
   if(glfwGetKey(pWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(pWindow, true);
   }
+}
+
+bool isShaderCompileSuccessful(const ObjID shaderId) {
+  int success{};
+  char infoLog[512];
+  
+  glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+  if(!success) {
+    glGetShaderInfoLog(shaderId, 512, nullptr, infoLog);
+    std::cerr << "Error on shader compilation: " << infoLog << std::endl;
+    return false;
+  }
+
+  return true;
+}
+
+bool isProgramLinkSuccessful(const ObjID programId) {
+  int success{};
+  char infoLog[512];
+
+  glGetProgramiv(programId, GL_LINK_STATUS, &success);
+  if(!success) {
+    glGetProgramInfoLog(programId, 512, nullptr, infoLog);
+    std::cerr << "Error on shader program linking: " << infoLog << std::endl;
+    return false;
+  }
+
+  return true;
 }
