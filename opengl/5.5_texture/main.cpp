81e7fe8555b06f0ef12c57797b07b1b049649116
Paul Galasti <pgalasti@gmail.com>
2026-08-13 20:40:39 -0400

    Testing out png with alpha

---
+++ opengl/5.5_texture/main.cpp
@@ -0,0 +1,131 @@
+#include "general/Math.h"
+#include "opengl/common/defines.h"
+#include "opengl/common/Helper.h"
+#include "opengl/common/OpenGLShaders.h"
+#include "opengl/common/Texture.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <fstream>
+#include <cstddef>
+
+using namespace GStuff::OpenGL;
+using namespace GStuff::OpenGL::Helper;
+using namespace GStuff::General::Shaders;
+using namespace GStuff::General::Math;
+
+void handleInput(GLFWwindow* pWindow);
+
+constexpr Vertex3DUVf vertices[] = {
+  { .vals = {-0.5f, -0.5f, 0.0f, 0.0f, 0.0f} }, // Bottom Left 
+  { .vals = { 0.5f, -0.5f, 0.0f, 1.0f, 0.0f} }, // Bottom Right 
+  { .vals = { 0.5f,  0.5f, 0.0f, 1.0f, 1.0f} }, // Top Right 
+  { .vals = { -0.5f, 0.5f, 0.0f, 0.0f, 1.0f} }  // Top Left 
+};
+
+constexpr unsigned int indices[] = {
+   0, 1, 2,
+   0, 3, 2
+};
+
+int main(int argc, char* argv[]) {
+
+#ifndef __APPLE__
+  // Wayland stuff makes life hard
+  glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* pWindow {glfwCreateWindow(800, 600, "texture", nullptr, nullptr)};
+  if(!pWindow) {
+    std::cerr << "Failed to create window!\n";
+    glfwTerminate();
+    return 1;
+  }
+  glfwMakeContextCurrent(pWindow);
+  glfwSetFramebufferSizeCallback(pWindow, [](GLFWwindow* pWindow, int width, int height) { glViewport(0, 0, width, height); });
+
+  glewExperimental = GL_TRUE;
+  if(glewInit() != GLEW_OK) {
+    std::cerr << "Failed to init GLEW\n";
+    return 1;
+  }
+  std::cout << "OpenGL Version Loaded: " << glGetString(GL_VERSION) << std::endl;
+
+  OpenGLProgram program {
+    std::make_unique<OpenGLShader>("./shaders/vs.glsl", Shader::ShaderType::Vertex),
+    std::make_unique<OpenGLShader>("./shaders/fs.glsl", Shader::ShaderType::Fragment)
+  }; 
+
+  ObjID VAO, VBO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+  
+  glBindVertexArray(VAO);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+ 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex3DUVf), static_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex3DUVf), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
+ 
+  int width, height, nChannels;
+  TextureData* textureData = stbi_load("./textures/lightning.png", &width, &height, &nChannels, 0);
+  if(!textureData) {
+    throw std::runtime_error("Unable to load texture data from file system!");
+  }
+
+  Texture texture(textureData, width, height, GL_RGBA);
+  stbi_image_free(textureData);
+  
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  program.Activate();
+  program.SetConstant("ourTexture", 0);
+
+  while(!glfwWindowShouldClose(pWindow)) {
+    handleInput(pWindow);
+    glfwPollEvents();
+
+    glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBindVertexArray(VAO);
+    
+    program.Activate();
+    texture.Bind(0);
+    
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    glfwSwapBuffers(pWindow);
+  }
+
+  glfwTerminate();
+  return 0;
+}
+
+void handleInput(GLFWwindow* pWindow) {
+  if(glfwGetKey(pWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(pWindow, true);
+  }
+}
