1b18453c5a7e6ea7b3cb2f7125a0d2f1f47698ea
Paul Galasti <pgalasti@gmail.com>
2026-08-16 13:17:59 -0400

    Setting up MVP

---
+++ opengl/7.2_transform_wvp/main.cpp
@@ -0,0 +1,165 @@
+#include "general/Math.h"
+#include "general/Profiler.h"
+#include "general/ModelLoader.h"
+
+#include "opengl/common/defines.h"
+#include "opengl/common/Helper.h"
+#include "opengl/common/OpenGLShaders.h"
+#include "opengl/common/Texture.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <fstream>
+#include <cstddef>
+#include <random>
+
+
+using namespace GStuff::OpenGL;
+using namespace GStuff::OpenGL::Helper;
+using namespace GStuff::General::Shaders;
+using namespace GStuff::General::Math;
+using namespace GStuff::General;
+
+using ModelLoader = WavefrontVertexLoader<Vertex3DRGBf>;
+
+
+void handleInput(GLFWwindow* pWindow);
+void addColors(ModelLoader::VertexData& vertices);
+
+int main([[maybe_unused]]int argc, [[maybe_unused]]char* argv[]) {
+
+#ifndef __APPLE__
+  // Wayland stuff makes life hard
+  glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+#endif
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* pWindow {glfwCreateWindow(800, 600, "texture mix", nullptr, nullptr)};
+  if(!pWindow) {
+    std::cerr << "Failed to create window!\n";
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(pWindow);
+  glfwSwapInterval(0); // Attempt to uncap VSync
+  glfwSetFramebufferSizeCallback(pWindow, []([[maybe_unused]]GLFWwindow* pWindow, int width, int height) { glViewport(0, 0, width, height); });
+
+  glewExperimental = GL_TRUE;
+  if(glewInit() != GLEW_OK) {
+    std::cerr << "Failed to init GLEW\n";
+    return 1;
+  }
+
+  glEnable(GL_DEPTH_TEST);
+  std::cout << "OpenGL Version Loaded: " << glGetString(GL_VERSION) << std::endl;
+
+  std::cout << "Loading model" << std::endl;
+  ProfilerMs profiler;
+ 
+  ModelLoader modelLoader(ModelLoader::Format::XYZRGB);
+  
+  profiler.Start("Loading Model");
+  const ModelLoader::ModelData modelData = modelLoader.Load("./models/cube.obj", ModelLoader::LOAD_INDICES);
+  const auto snapshot {profiler.Stop()};
+  ModelLoader::VertexData vertices{modelData.first};
+  const ModelLoader::IndexData indices{modelData.second};
+  if(vertices.empty() || indices.empty()) {
+    throw std::runtime_error("Unable to load vertices/indices of model!");
+  }
+
+  addColors(vertices);
+  std::cout << snapshot << std::endl;
+
+  const std::size_t vertexByteSize{vertices.size()*sizeof(vertices[0])};
+  const std::size_t indexByteSize{indices.size()*sizeof(indices[0])};
+
+  OpenGLProgram program {
+    std::make_unique<OpenGLShader>("./shaders/vs.glsl", Shader::ShaderType::Vertex),
+    std::make_unique<OpenGLShader>("./shaders/fs.glsl", Shader::ShaderType::Fragment)
+  }; 
+
+  ObjID VAO, VBO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+  
+  glBindVertexArray(VAO);
+  
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, vertexByteSize, vertices.data(), GL_STATIC_DRAW);
+ 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexByteSize, indices.data(), GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex3DRGBf), static_cast<void*>(0));
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex3DRGBf), (void*)(sizeof(float)*3));
+  glEnableVertexAttribArray(1);
+
+  program.Activate();
+
+  while(!glfwWindowShouldClose(pWindow)) {
+    handleInput(pWindow);
+    glfwPollEvents();
+
+    glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glm::mat4 transformationMtx {glm::mat4(1.0f)};
+    transformationMtx = glm::scale(transformationMtx, glm::vec3(0.25f, 0.25f, 0.25f));
+    transformationMtx = glm::rotate(transformationMtx, static_cast<float>(glfwGetTime()), glm::vec3(1.0f, 1.0f, 1.0f));
+    SetTransformation(program.ProgramID(), "transform", transformationMtx);
+
+    glBindVertexArray(VAO);
+    
+    program.Activate();
+
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+
+    glBindVertexArray(0);
+
+    glfwSwapBuffers(pWindow);
+  }
+  
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  glfwTerminate();
+  return 0;
+}
+
+void handleInput(GLFWwindow* pWindow) {
+  if(glfwGetKey(pWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(pWindow, true);
+  }
+}
+
+void addColors(ModelLoader::VertexData& vertices) {
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<float> distribution(0.0f, 1.0f);
+
+
+  for(auto& vertex : vertices) {
+    vertex.r = distribution(gen);
+    vertex.g = distribution(gen);
+    vertex.b = distribution(gen);
+  }
+}
---
+++ opengl/7.2_transform_wvp/shaders/vs.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+
+uniform mat4 transform;
+
+out vec3 inputColor; 
+
+void main() { 
+  gl_Position = transform * vec4(aPos, 1.0);
+  inputColor = aColor;
+}
