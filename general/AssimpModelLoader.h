db7d7e70abd417c057ff9a53d0e491d8416996e1
Paul Galasti <pgalasti@gmail.com>
2026-09-04 16:11:21 -0400

    Minor fixes for assimp setup

--- general/AssimpModelLoader.h
+++ general/AssimpModelLoader.h
@@ -6,6 +6,8 @@
 #include <utility>
 #include <vector>
 #include <string_view>
+#include <string>
+#include <sstream>
 #include <stdexcept>
 #include <iostream>
 
@@ -15,7 +17,7 @@
 
 namespace GStuff::General::ModelLoad {
 
-const* aiScene ReadScene(std::string_view filePath);
+const aiScene* ReadScene(std::string_view filePath);
 
 	
 using namespace GStuff::General::Math;
@@ -33,15 +35,17 @@ Vertex3DNUVfData Load(std::string_view filePath) {
 
 
 // Assimp specific functions
-const* aiScene ReadScene(std::string_view filePath) {
+const aiScene* ReadScene(std::string_view filePath) {
   Assimp::Importer importer;
 
   constexpr unsigned int flags = 
-    (ai_Process_Triangulate | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
-  const aiScene* pScene { importer.ReadFile(filePath, flags) }; 
+    (aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
+  const aiScene* pScene { importer.ReadFile(std::string(filePath), flags) }; 
 
   if(!pScene || pScene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !pScene->mRootNode) {
-    std::cerr << "Error importing filePath " + filePath + " for Assip::Importer::ReadFile!" << std::endl;
+    std::stringstream ss("Error importing filePath ");
+    ss << filePath << " for Assip::Importer::ReadFile!" << std::endl;
+    std::cerr << ss.str();
     std::cerr << importer.GetErrorString() << std::endl;
     throw std::runtime_error("Unable to load model via Assip importer!");
   }
